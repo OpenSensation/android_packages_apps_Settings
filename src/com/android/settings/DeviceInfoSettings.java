2ff4e1456960981eabe71877e84419290f93ac06
Philipp 'TamCore' B <philipp@tamcore.eu>
2012-10-07 02:24:25 +0200

    Include CM version in Settings > About
    So users can keep track of the current CM versions

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -64,7 +64,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
     private static final String KEY_BASEBAND_VERSION = "baseband_version";
     private static final String KEY_FIRMWARE_VERSION = "firmware_version";
     private static final String KEY_UPDATE_SETTING = "additional_system_update_settings";
-    private static final String KEY_MOD_VERSION = "mod_version";
+    private static final String KEY_CYANOGENMOD_VERSION = "cm_version";
+    private static final String KEY_OPENSENSATION_VERSION = "os_version";
     private static final String KEY_MOD_BUILD_DATE = "build_date";
     private static final String KEY_DEVICE_CPU = "device_cpu";
     private static final String KEY_DEVICE_MEMORY = "device_memory";
@@ -88,8 +89,9 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
         setStringSummary(KEY_DEVICE_MODEL, Build.MODEL);
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
-        setValueSummary(KEY_MOD_VERSION, "ro.cm.version");
-        findPreference(KEY_MOD_VERSION).setEnabled(true);
+        setValueSummary(KEY_CYANOGENMOD_VERSION, "ro.cm.version");
+        setValueSummary(KEY_OPENSENSATION_VERSION, "ro.os.version");
+        findPreference(KEY_OPENSENSATION_VERSION).setEnabled(true);
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
 
         String cpuInfo = getCPUInfo();
@@ -170,7 +172,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
                 }
             }
         }
-        if (preference.getKey().equals(KEY_MOD_VERSION)) {
+        if (preference.getKey().equals(KEY_OPENSENSATION_VERSION)) {
             System.arraycopy(mHits, 1, mHits, 0, mHits.length-1);
             mHits[mHits.length-1] = SystemClock.uptimeMillis();
             if (mHits[0] >= (SystemClock.uptimeMillis()-500)) {
